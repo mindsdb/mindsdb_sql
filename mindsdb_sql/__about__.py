5dcf7316a1b494e6399d7d11133d8d33f5649af0
Boris <b.tseytlin@lambda-it.ru>
2021-06-02 18:02:55 +0300

    Version bump

--- mindsdb_sql/__about__.py
+++ mindsdb_sql/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'mindsdb_sql'
 __package_name__ = 'mindsdb_sql'
-__version__ = '0.0.10'
+__version__ = '0.0.11'
 __description__ = "Pure python SQL parser"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
