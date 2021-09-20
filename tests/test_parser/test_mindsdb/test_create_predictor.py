ab0c02418e71ab222d28408558da6257eeb5952c
Boris <b.tseytlin@lambda-it.ru>
2021-09-20 13:00:25 +0300

    Add more tests for create predictor

--- tests/test_parser/test_mindsdb/test_create_predictor.py
+++ tests/test_parser/test_mindsdb/test_create_predictor.py
@@ -7,7 +7,7 @@ from mindsdb_sql.utils import to_single_line
 
 
 class TestCreatePredictor:
-    def test_create_predictor_ok(self):
+    def test_create_predictor_full(self):
         sql = """CREATE PREDICTOR pred
                 FROM integration_name 
                 WITH ('select * FROM table') 
@@ -38,3 +38,38 @@ class TestCreatePredictor:
         assert str(ast).lower() == to_single_line(sql.lower())
         assert to_single_line(str(ast)) == to_single_line(str(expected_ast))
         assert ast.to_tree() == expected_ast.to_tree()
+
+    def test_create_predictor_minimal(self):
+        sql = """CREATE PREDICTOR pred
+                FROM integration_name 
+                WITH ('select * FROM table') 
+                AS ds_name
+                PREDICT f1 as f1_alias, f2
+                """
+        ast = parse_sql(sql, dialect='mindsdb')
+        expected_ast = CreatePredictor(
+            name='pred',
+            integration_name='integration_name',
+            query='select * FROM table',
+            datasource_name='ds_name',
+            targets=[Identifier('f1', alias=Identifier('f1_alias')),
+                             Identifier('f2')],
+        )
+        assert str(ast).lower() == to_single_line(sql.lower())
+        assert to_single_line(str(ast)) == to_single_line(str(expected_ast))
+        assert ast.to_tree() == expected_ast.to_tree()
+
+    def test_create_predictor_invalid_json(self):
+        sql = """CREATE PREDICTOR pred
+                FROM integration_name 
+                WITH ('select * FROM table') 
+                AS ds_name
+                PREDICT f1 as f1_alias, f2
+                ORDER BY f_order_1 ASC, f_order_2, f_order_3 DESC
+                GROUP BY f_group_1, f_group_2
+                WINDOW 100
+                HORIZON 7
+                USING 'not_really_json'"""
+
+        with pytest.raises(ParsingException):
+            parse_sql(sql, dialect='mindsdb')
