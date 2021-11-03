4fb23a930cb0d119b2737add26fba8e2bbbc5460
Boris <b.tseytlin@lambda-it.ru>
2021-11-03 16:26:41 +0300

    Add mindsdb predict

--- tests/test_parser/test_mindsdb/test_selects.py
+++ tests/test_parser/test_mindsdb/test_selects.py
@@ -5,7 +5,7 @@ from mindsdb_sql.parser.dialects.mindsdb import *
 from mindsdb_sql.parser.ast import *
 
 
-class TestSelectPredictors:
+class TestSpecificSelects:
     def test_select_from_predictors(self):
         sql = "SELECT * FROM predictors WHERE name = 'pred_name'"
         ast = parse_sql(sql, dialect='mindsdb')
@@ -13,6 +13,18 @@ class TestSelectPredictors:
             targets=[Star()],
             from_table=Identifier('predictors'),
             where=BinaryOperation('=', args=[Identifier('name'), Constant('pred_name')])
+        )
+
+        assert str(ast).lower() == sql.lower()
+        assert str(ast) == str(expected_ast)
+        assert ast.to_tree() == expected_ast.to_tree()
+
+    def test_select_predict_column(self):
+        sql = "SELECT predict FROM mindsdb.predictors"
+        ast = parse_sql(sql, dialect='mindsdb')
+        expected_ast = Select(
+            targets=[Identifier('predict')],
+            from_table=Identifier('mindsdb.predictors'),
         )
 
         assert str(ast).lower() == sql.lower()
