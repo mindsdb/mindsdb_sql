ffb3b76b714b32cca0f3199cf796d509f0c026d2
Boris <b.tseytlin@lambda-it.ru>
2021-11-17 13:04:57 +0300

    Add test for join on tables followed by group by

--- tests/test_executioner/test_executioner.py
+++ tests/test_executioner/test_executioner.py
@@ -190,6 +190,38 @@ class TestExecuteSelectFromIntegration:
         assert (out_df['App'] == expected_df['App']).all()
         np.testing.assert_allclose(out_df['avg_sentiment_polarity'].values, expected_df['avg_sentiment_polarity'].values, atol=1e-5)
 
+    def test_execute_join_tables_with_groupby_having(self, connection_db1, default_data_db1, connection_db2, default_data_db2):
+        sql = """
+            SELECT test_db1.App, test_db1.Sentiment, avg(CAST(test_db1.Sentiment_Polarity AS float)) AS avg_sentiment_polarity
+            FROM test_db1.googleplaystore AS t1
+            INNER JOIN test_db2.googleplaystore_user_reviews AS t2
+            ON t1.App = t2.App
+            GROUP BY test_db1.App
+            HAVING avg_sentiment_polarity > 0.4
+        """
+
+        df1 = connection_db1.query("SELECT App, Category, Rating FROM googleplaystore")
+        df2 = connection_db2.query("SELECT App, Sentiment, Sentiment_Polarity FROM googleplaystore_user_reviews")
+        inner_df = df1.merge(df2, on=['App'], how='inner')
+        inner_df['Sentiment_Polarity'] = inner_df['Sentiment_Polarity'].astype(float)
+        expected_df = inner_df.groupby(['App']).agg({'Sentiment_Polarity': 'mean'}).reset_index()
+        expected_df.columns = ['App', 'avg_sentiment_polarity']
+        expected_df = expected_df[expected_df['avg_sentiment_polarity'] > 0.4]
+
+        query = parse_sql(sql, dialect='mindsdb')
+        assert str(query) == to_single_line(sql)
+        query_plan = plan_query(query, integrations=['test_db1', 'test_db2'])
+        out_df = execute_plan(query_plan,
+                           integration_connections=dict(
+                               test_db1=connection_db1,
+                               test_db2=connection_db2,
+                           ))
+
+        assert out_df.shape == expected_df.shape
+        assert (out_df.columns == expected_df.columns).all()
+        assert (out_df == expected_df).all().all()
+
+
     def test_execute_join_tables_with_subquery_groupby_having(self, connection_db1, default_data_db1, connection_db2, default_data_db2):
         sql = """
             SELECT App, avg(Sentiment_Polarity) AS avg_sentiment_polarity
