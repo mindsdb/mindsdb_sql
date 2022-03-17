9066d2d934eb41e496861f1df41a845d7daa4fda
Elkin Andrey <andrey@mindsdb.com>
2022-03-17 17:15:53 +0300

    bump version

--- mindsdb_sql/__about__.py
+++ mindsdb_sql/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'mindsdb_sql'
 __package_name__ = 'mindsdb_sql'
-__version__ = '0.1.3'
+__version__ = '0.2.0'
 __description__ = "Pure python SQL parser"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
