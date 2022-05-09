4f847fdf76124e834d7bb17c5a8817c8e2589325
Patricio Cerda Mardini <pcerdam@live.com>
2022-05-09 19:25:10 +0200

    fix M#2180

--- tests/test_parser/test_mindsdb/test_timeseries.py
+++ tests/test_parser/test_mindsdb/test_timeseries.py
@@ -2,9 +2,10 @@ from mindsdb_sql import parse_sql
 from mindsdb_sql.parser.ast import *
 from mindsdb_sql.parser.dialects.mindsdb.latest import Latest
 from mindsdb_sql.parser.utils import JoinType
+from mindsdb_sql.planner.ts_utils import validate_ts_where_condition
 
 
-class TestCreateView:
+class TestTimeSeries:
     def test_latest_in_where(self):
         sql = "SELECT time, price FROM crypto INNER JOIN pred WHERE time > LATEST"
         ast = parse_sql(sql, dialect='mindsdb')
@@ -19,3 +20,11 @@ class TestCreateView:
         assert str(ast).lower() == sql.lower()
         assert str(ast) == str(expected_ast)
         assert ast.to_tree() == expected_ast.to_tree()
+
+    def test_multi_groupby(self):
+        sql = """SELECT m.saledate as date, m.MA as forecast FROM mindsdb.home_sales_model as m 
+                 JOIN files.HR_MA as t WHERE t.saledate > LATEST AND t.type = 'house' AND t.bedrooms = 2 LIMIT 4;"""
+        ast = parse_sql(sql, dialect='mindsdb')
+        allowed_cols = ['type', 'bedrooms',  # groupby
+                        'saledate']          # orderby
+        validate_ts_where_condition(ast.where, allowed_columns=allowed_cols)
