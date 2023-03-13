39068152e1618e1252371326205722cd3b3d7abf
Andrew Potter <hello@drewpotter.com>
2023-03-13 15:29:08 +0000

    Remove duplicate test

--- tests/test_parser/test_mysql/test_mysql_parser.py
+++ tests/test_parser/test_mysql/test_mysql_parser.py
@@ -64,19 +64,6 @@ class TestMySQLParser:
         assert str(ast) == str(expected_ast)
         assert ast.to_tree() == expected_ast.to_tree()
 
-
-    def test_names_collate_utf8(self):
-        sql = "SET sql_mode=(SELECT CONCAT(@@sql_mode, ',PIPES_AS_CONCAT,NO_ENGINE_SUBSTITUTION')),time_zone='+00:00',NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
-        ast = parse_sql(sql, dialect='mysql')
-        expected_ast = Show(
-            category='INDEX',
-            from_table=Identifier('db.predictors'),
-        )
-
-        # assert str(ast).lower() == sql.lower()
-        assert str(ast) == str(expected_ast)
-        assert ast.to_tree() == expected_ast.to_tree()
-
     def test_names_collate_utf8_set(self):
         sql = "SET sql_mode=(SELECT CONCAT(@@sql_mode, ',PIPES_AS_CONCAT,NO_ENGINE_SUBSTITUTION')),time_zone='+00:00',NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
         ast = parse_sql(sql, dialect='mysql')
@@ -88,4 +75,3 @@ class TestMySQLParser:
         # assert str(ast).lower() == sql.lower()
         assert str(ast) == str(expected_ast)
         assert ast.to_tree() == expected_ast.to_tree()
-    
