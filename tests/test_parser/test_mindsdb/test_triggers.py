2f874a5949f2dad0b14154ca5f81532787dbc465
Elkin Andrey <andrey@mindsdb.com>
2023-07-17 16:20:44 +0300

    tests triggers

---
+++ tests/test_parser/test_mindsdb/test_triggers.py
@@ -0,0 +1,32 @@
+from mindsdb_sql import parse_sql
+from mindsdb_sql.parser.dialects.mindsdb import *
+from mindsdb_sql.parser.ast import *
+
+
+class TestTriggers:
+    def test_create_trigger(self):
+        sql = '''
+            create trigger proj2.tname on db1.tbl1 
+             (
+                 retrain p1
+            ) 
+        '''
+        ast = parse_sql(sql, dialect='mindsdb')
+        expected_ast = CreateTrigger(
+            name=Identifier('proj2.tname'),
+            table=Identifier('db1.tbl1'),
+            query_str="retrain p1",
+        )
+        assert str(ast) == str(expected_ast)
+        assert ast.to_tree() == expected_ast.to_tree()
+
+    def test_drop_trigger(self):
+        sql = '''
+            drop trigger proj1.tname
+        '''
+        ast = parse_sql(sql, dialect='mindsdb')
+        expected_ast = DropTrigger(
+            name=Identifier('proj1.tname'),
+        )
+        assert str(ast) == str(expected_ast)
+        assert ast.to_tree() == expected_ast.to_tree()
