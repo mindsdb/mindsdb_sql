b976b1ed389f5da4f89a1ac21f8e3e5b3a8a57ff
Zoran Pandovski <zoran.pandovski@gmail.com>
2023-09-06 16:15:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,3 @@ if __name__ == '__main__':
         if text:
             for tok in lexer.tokenize(text):
                 print('type=%r, value=%r' % (tok.type, tok.value))
-
-            # parser.parse(lexer.tokenize(text))
