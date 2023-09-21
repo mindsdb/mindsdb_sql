e4b991844005c19a8776f84fedb4f8f86063e80a
Elkin Andrey <andrey@mindsdb.com>
2023-09-21 11:35:13 +0300

    added test

--- tests/test_parser/test_base_sql/test_base_sql.py
+++ tests/test_parser/test_base_sql/test_base_sql.py
@@ -1,6 +1,9 @@
 import pytest
 from mindsdb_sql import parse_sql
 
+from mindsdb_sql.parser.ast import *
+
+
 class TestSql:
     def test_ending(self):
         sql = """INSERT INTO tbl_name VALUES (1, 3)  
@@ -8,3 +11,24 @@ class TestSql:
         """
 
         parse_sql(sql, dialect='mindsdb')
+
+    def test_not_equal(self):
+        sql = " select * from t1 where a<>1"
+
+        ast = parse_sql(sql, dialect='mindsdb')
+
+        expected_ast = Select(
+            targets=[Star()],
+            from_table=Identifier('t1'),
+            where=BinaryOperation(
+                op='<>',
+                args=[
+                    Identifier('a'),
+                    Constant(11)
+                ]
+            )
+        )
+
+        assert str(ast).lower() == str(expected_ast).lower()
+        assert ast.to_tree() == expected_ast.to_tree()
+
