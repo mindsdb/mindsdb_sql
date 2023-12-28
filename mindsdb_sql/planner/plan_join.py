cb09b8aa7c9bae3673d38529a7361f65831df1f0
andrew <elkin.andr@gmail.com>
2023-12-28 12:26:48 +0300

    split join to functions

--- mindsdb_sql/planner/plan_join.py
+++ mindsdb_sql/planner/plan_join.py
@@ -24,7 +24,11 @@ class PlanJoin:
     def __init__(self, planner):
         self.planner = planner
 
+        # index to lookup tables
         self.tables_idx = None
+
+        self.step_stack = None
+        self.query_context = {}
 
     def plan(self, query):
         self.tables_idx = {}
@@ -59,7 +63,6 @@ class PlanJoin:
         sub_select = getattr(table, 'sub_select', None)
 
         return TableInfo(integration, table, aliases, conditions=[], sub_select=sub_select)
-
 
     def get_table_for_column(self, column: Identifier):
 
@@ -103,7 +106,7 @@ class PlanJoin:
             raise NotImplementedError()
         return sequence
 
-    def _check_condition_node(self, node):
+    def check_node_condition(self, node):
 
         col_idx = 0
         if len(node.args) == 2:
@@ -139,6 +142,41 @@ class PlanJoin:
         node2._orig_node = node
         table_info.conditions.append(node2)
 
+    def check_query_conditions(self, query):
+        # get conditions for tables
+        binary_ops = []
+
+        def _check_node_condition(node, **kwargs):
+            if isinstance(node, BetweenOperation):
+                self.check_node_condition(node)
+
+            if isinstance(node, BinaryOperation):
+                binary_ops.append(node.op)
+
+                self.check_node_condition(node)
+
+        query_traversal(query.where, _check_node_condition)
+
+        self.query_context['binary_ops'] = binary_ops
+
+    def check_use_limit(self, query_in, join_sequence):
+        # use limit for first table?
+        # if only models
+        use_limit = False
+        if query_in.having is None or query_in.group_by is None and query_in.limit is not None:
+
+            join = None
+            use_limit = True
+            for item in join_sequence:
+                if isinstance(item, TableInfo):
+                    if item.predictor_info is None and item.sub_select is None:
+                        if join is not None:
+                            if join.join_type.upper() != 'LEFT JOIN':
+                                use_limit = False
+                elif isinstance(item, Join):
+                    join = item
+        self.query_context['use_limit'] = use_limit
+
     def plan_join_tables(self, query_in):
         query = copy.deepcopy(query_in)
 
@@ -157,11 +195,10 @@ class PlanJoin:
 
         query_traversal(query.from_table, replace_subselects)
 
-
         # get all join tables, form join sequence
-
         join_sequence = self.get_join_sequence(query.from_table)
 
+        # find tables for identifiers used in query
         def _check_identifiers(node, is_table, **kwargs):
             if not is_table and isinstance(node, Identifier):
                 if len(node.parts) > 1:
@@ -176,143 +213,36 @@ class PlanJoin:
 
         query_traversal(query, _check_identifiers)
 
-
+        # plan all subselects in query
         find_selects = self.planner.get_nested_selects_plan_fnc(self.planner.default_namespace, force=True)
         query_traversal(query.where, find_selects)
 
-        # get conditions for tables
-        binary_ops = []
-
-        def _check_condition(node, **kwargs):
-            if isinstance(node, BetweenOperation):
-                self._check_condition_node(node)
-
-            if isinstance(node, BinaryOperation):
-                binary_ops.append(node.op)
-
-                self._check_condition_node(node)
-
-        query_traversal(query.where, _check_condition)
+        self.check_query_conditions(query)
 
         # workaround for 'model join table': swap tables:
         if len(join_sequence) == 3 and join_sequence[0].predictor_info is not None:
             join_sequence = [join_sequence[1], join_sequence[0], join_sequence[2]]
 
-        # use limit for first table?
-        # if only models
-        use_limit = False
-        if query_in.having is None or query_in.group_by is None and query_in.limit is not None:
-
-            join = None
-            use_limit = True
-            for item in join_sequence:
-                if isinstance(item, TableInfo):
-                    if item.predictor_info is None and item.sub_select is None:
-                        if join is not None:
-                            if join.join_type.upper() != 'LEFT JOIN':
-                                use_limit = False
-                elif isinstance(item, Join):
-                    join = item
+        self.check_use_limit(query_in, join_sequence)
 
         # create plan
         # TODO add optimization: one integration without predictor
 
-        step_stack = []
+        self.step_stack = []
         for item in join_sequence:
             if isinstance(item, TableInfo):
 
                 if item.sub_select is not None:
-                    # is sub select
-                    item.sub_select.alias = None
-                    item.sub_select.parentheses = False
-                    step = self.planner.plan_select(item.sub_select)
-
-                    where = filters_to_bin_op(item.conditions)
-
-                    # apply table alias
-                    query2 = Select(targets=[Star()], where=where)
-                    if item.table.alias is None:
-                        raise PlanningException(f'Subselect in join have to be aliased: {item.sub_select.to_string()}')
-                    table_name = item.table.alias.parts[-1]
-
-                    add_absent_cols = False
-                    if hasattr (item.sub_select, 'from_table') and\
-                         isinstance(item.sub_select.from_table, ast.Data):
-                        add_absent_cols = True
-
-                    step2 = SubSelectStep(query2, step.result, table_name=table_name, add_absent_cols=add_absent_cols)
-                    step2 = self.planner.plan.add_step(step2)
-                    step_stack.append(step2)
+                    self.process_subselect(item)
                 elif item.predictor_info is not None:
-                    if len(step_stack) == 0:
-                        raise NotImplementedError("Predictor can't be first element of join syntax")
-                    if item.predictor_info.get('timeseries'):
-                        raise NotImplementedError("TS predictor is not supported here yet")
-                    data_step = step_stack[-1]
-                    row_dict = None
-                    if item.conditions:
-                        row_dict = {}
-                        for el in item.conditions:
-                            if isinstance(el.args[0], Identifier) and el.op == '=':
-
-                                if isinstance(el.args[1], (Constant, Parameter)):
-                                    row_dict[el.args[0].parts[-1]] = el.args[1].value
-
-                                # exclude condition
-                                item.conditions[0]._orig_node.args = [Constant(0), Constant(0)]
-
-                    predictor_step = self.planner.plan.add_step(ApplyPredictorStep(
-                        namespace=item.integration,
-                        dataframe=data_step.result,
-                        predictor=item.table,
-                        params=query.using,
-                        row_dict=row_dict,
-                    ))
-                    step_stack.append(predictor_step)
+                    self.process_predictor(item, query_in)
                 else:
                     # is table
-                    query2 = Select(from_table=item.table, targets=[Star()])
-                    # parts = tuple(map(str.lower, table_name.parts))
-                    conditions = item.conditions
-                    if 'or' in binary_ops:
-                        # not use conditions
-                        conditions = []
+                    self.process_table(item, query_in)
 
-                    if use_limit:
-                        order_by = None
-                        if query_in.order_by is not None:
-                            order_by = []
-                            # all order column be from this table
-                            for col in query_in.order_by:
-                                if self.get_table_for_column(col.field).table != item.table:
-                                    order_by = False
-                                col = copy.deepcopy(col)
-                                col.field.parts = [col.field.parts[-1]]
-                                order_by.append(col)
-
-                        if order_by is not False:
-                            # copy limit from upper query
-                            query2.limit = query_in.limit
-                            # move offset from upper query
-                            query2.offset = query_in.offset
-                            query_in.offset = None
-                            # copy order
-                            query2.order_by = order_by
-
-                        use_limit = False
-                    for cond in conditions:
-                        if query2.where is not None:
-                            query2.where = BinaryOperation('and', args=[query2.where, cond])
-                        else:
-                            query2.where = cond
-
-                    # step = self.planner.get_integration_select_step(query2)
-                    step = FetchDataframeStep(integration=item.integration, query=query2)
-                    self.planner.plan.add_step(step)
-                    step_stack.append(step)
             elif isinstance(item, Join):
-                step_right = step_stack.pop()
-                step_left = step_stack.pop()
+                step_right = self.step_stack.pop()
+                step_left = self.step_stack.pop()
 
                 new_join = copy.deepcopy(item)
 
@@ -323,7 +253,98 @@ class PlanJoin:
 
                 step = self.planner.plan.add_step(JoinStep(left=step_left.result, right=step_right.result, query=new_join))
 
-                step_stack.append(step)
+                self.step_stack.append(step)
 
         query_in.where = query.where
-        return step_stack.pop()
+        return self.step_stack.pop()
+
+    def process_subselect(self, item):
+        # is sub select
+        item.sub_select.alias = None
+        item.sub_select.parentheses = False
+        step = self.planner.plan_select(item.sub_select)
+
+        where = filters_to_bin_op(item.conditions)
+
+        # apply table alias
+        query2 = Select(targets=[Star()], where=where)
+        if item.table.alias is None:
+            raise PlanningException(f'Subselect in join have to be aliased: {item.sub_select.to_string()}')
+        table_name = item.table.alias.parts[-1]
+
+        add_absent_cols = False
+        if hasattr(item.sub_select, 'from_table') and \
+                isinstance(item.sub_select.from_table, ast.Data):
+            add_absent_cols = True
+
+        step2 = SubSelectStep(query2, step.result, table_name=table_name, add_absent_cols=add_absent_cols)
+        step2 = self.planner.plan.add_step(step2)
+        self.step_stack.append(step2)
+
+    def process_table(self, item, query_in):
+        query2 = Select(from_table=item.table, targets=[Star()])
+        # parts = tuple(map(str.lower, table_name.parts))
+        conditions = item.conditions
+        if 'or' in self.query_context['binary_ops']:
+            # not use conditions
+            conditions = []
+
+        if self.query_context['use_limit']:
+            order_by = None
+            if query_in.order_by is not None:
+                order_by = []
+                # all order column be from this table
+                for col in query_in.order_by:
+                    if self.get_table_for_column(col.field).table != item.table:
+                        order_by = False
+                    col = copy.deepcopy(col)
+                    col.field.parts = [col.field.parts[-1]]
+                    order_by.append(col)
+
+            if order_by is not False:
+                # copy limit from upper query
+                query2.limit = query_in.limit
+                # move offset from upper query
+                query2.offset = query_in.offset
+                query_in.offset = None
+                # copy order
+                query2.order_by = order_by
+
+            self.query_context['use_limit'] = False
+        for cond in conditions:
+            if query2.where is not None:
+                query2.where = BinaryOperation('and', args=[query2.where, cond])
+            else:
+                query2.where = cond
+
+        # step = self.planner.get_integration_select_step(query2)
+        step = FetchDataframeStep(integration=item.integration, query=query2)
+        self.planner.plan.add_step(step)
+        self.step_stack.append(step)
+
+    def process_predictor(self, item, query_in):
+        if len(self.step_stack) == 0:
+            raise NotImplementedError("Predictor can't be first element of join syntax")
+        if item.predictor_info.get('timeseries'):
+            raise NotImplementedError("TS predictor is not supported here yet")
+        data_step = self.step_stack[-1]
+        row_dict = None
+        if item.conditions:
+            row_dict = {}
+            for el in item.conditions:
+                if isinstance(el.args[0], Identifier) and el.op == '=':
+
+                    if isinstance(el.args[1], (Constant, Parameter)):
+                        row_dict[el.args[0].parts[-1]] = el.args[1].value
+
+                    # exclude condition
+                    item.conditions[0]._orig_node.args = [Constant(0), Constant(0)]
+
+        predictor_step = self.planner.plan.add_step(ApplyPredictorStep(
+            namespace=item.integration,
+            dataframe=data_step.result,
+            predictor=item.table,
+            params=query_in.using,
+            row_dict=row_dict,
+        ))
+        self.step_stack.append(predictor_step)
