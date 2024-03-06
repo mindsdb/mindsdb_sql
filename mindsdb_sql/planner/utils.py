bf0820265df240c9d7d9bc0fab2fb36102ed81e8
andrew <elkin.andr@gmail.com>
2024-03-06 17:22:09 +0300

    return node in query_traversal. it helps for options like 'where a=1' (only one binary operation, and we want to replace it) https://github.com/mindsdb/mindsdb/pull/8867

--- mindsdb_sql/planner/utils.py
+++ mindsdb_sql/planner/utils.py
@@ -299,6 +299,8 @@ def query_traversal(node, callback, is_table=False, is_target=False, parent_quer
             array.append(node_out)
         return array
 
+    return node
+
 
 def convert_join_to_list(join):
     # join tree to table list
