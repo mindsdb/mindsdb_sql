d79cf771b4653413d9288cb62e3e7a4637ef9a94
andrew <elkin.andr@gmail.com>
2024-10-30 15:34:33 +0300

    tests for intersect, except

--- tests/test_parser/test_base_sql/test_union.py
+++ tests/test_parser/test_base_sql/test_union.py
@@ -11,38 +11,40 @@ class TestUnion:
             parse_sql(sql)
 
     def test_union_base(self):
-        sql = """SELECT col1 FROM tab1 
-        UNION 
-        SELECT col1 FROM tab2"""
+        for keyword, cls in {'union': Union, 'intersect': Intersect, 'except': Except}.items():
+            sql = f"""SELECT col1 FROM tab1 
+            {keyword} 
+            SELECT col1 FROM tab2"""
 
-        ast = parse_sql(sql)
-        expected_ast = Union(unique=True,
-                             left=Select(targets=[Identifier('col1')],
-                                         from_table=Identifier(parts=['tab1']),
-                                         ),
-                             right=Select(targets=[Identifier('col1')],
-                                         from_table=Identifier(parts=['tab2']),
-                                         ),
-                             )
-        assert ast.to_tree() == expected_ast.to_tree()
-        assert str(ast) == str(expected_ast)
+            ast = parse_sql(sql)
+            expected_ast = cls(unique=True,
+                                 left=Select(targets=[Identifier('col1')],
+                                             from_table=Identifier(parts=['tab1']),
+                                             ),
+                                 right=Select(targets=[Identifier('col1')],
+                                             from_table=Identifier(parts=['tab2']),
+                                             ),
+                                 )
+            assert ast.to_tree() == expected_ast.to_tree()
+            assert str(ast) == str(expected_ast)
 
     def test_union_all(self):
-        sql = """SELECT col1 FROM tab1 
-        UNION ALL
-        SELECT col1 FROM tab2"""
+        for keyword, cls in {'union': Union, 'intersect': Intersect, 'except': Except}.items():
+            sql = f"""SELECT col1 FROM tab1 
+            {keyword} ALL
+            SELECT col1 FROM tab2"""
 
-        ast = parse_sql(sql)
-        expected_ast = Union(unique=False,
-                             left=Select(targets=[Identifier('col1')],
-                                         from_table=Identifier(parts=['tab1']),
-                                         ),
-                             right=Select(targets=[Identifier('col1')],
-                                         from_table=Identifier(parts=['tab2']),
-                                         ),
-                             )
-        assert ast.to_tree() == expected_ast.to_tree()
-        assert str(ast) == str(expected_ast)
+            ast = parse_sql(sql)
+            expected_ast = cls(unique=False,
+                                 left=Select(targets=[Identifier('col1')],
+                                             from_table=Identifier(parts=['tab1']),
+                                             ),
+                                 right=Select(targets=[Identifier('col1')],
+                                             from_table=Identifier(parts=['tab2']),
+                                             ),
+                                 )
+            assert ast.to_tree() == expected_ast.to_tree()
+            assert str(ast) == str(expected_ast)
 
     def xtest_union_alias(self):
         sql = """SELECT * FROM (
